87dfbd5119766ba7317477802e1c4f8834fc58f7
Deni <29742498+azureswastika@users.noreply.github.com>
2020-11-07 23:52:04 +0300

    Add .env parse

--- myconfig/main.py
+++ myconfig/main.py
@@ -1,12 +1,14 @@
-from json import load as j_load
+import json
 from json.decoder import JSONDecodeError
 from pathlib import Path
 from typing import Any
+from re import search
 
-from toml import load as t_laod
-from yaml import safe_load as y_load
+import toml
+import yaml
 
 DIR = Path.cwd()
+ENV = DIR.joinpath('.env')
 
 
 class MyConfig():
@@ -31,26 +33,58 @@ def parser(files: list) -> dict:
         dict: Parsed data.
     '''
 
-    format = files[0].split('.')[1]
+    for i, file in enumerate(files):
+        files[i] = str(DIR.joinpath(file))
     data = dict()
-    try:
-        if format == 'json':
-            for file in files:
-                with open(str(DIR.joinpath(file))) as f:
+    for file in files:
+        file_data = dict()
+        try:
+            with open(file, encoding='utf-8') as f:
+                format = file.split('.')[-1]
+                if format == 'json':
                     try:
-                        data.update(j_load(f))
+                        file_data = json.load(f)
                     except JSONDecodeError:
                         pass
-        elif format == 'toml':
-            for file in files:
-                with open(str(DIR.joinpath(file))) as f:
-                    data.update(t_laod(f))
-        elif format == 'yaml':
-            for file in files:
-                with open(str(DIR.joinpath(file))) as f:
-                    data.update(y_load(f))
-        else:
-            raise Exception('Unknown format.')
-    except FileNotFoundError:
-        raise Exception('Specify the correct path to the files.')
+                elif format == 'toml':
+                    file_data = toml.load(f)
+                elif format == 'yaml':
+                    file_data = yaml.safe_load(f)
+                data.update(file_data)
+        except FileNotFoundError:
+            print(
+                'File not found. ' +
+                'Specify the correct path to the file: {}'.format(
+                    file.split('\\')[-1]))
+    if ENV.exists():
+        env_data = env_parse(ENV)
+        data.update(env_data)
     return data
+
+
+def env_parse(file: str) -> dict:
+    '''Parses the .env file.
+
+    Args:
+        file (str): File path
+
+    Returns:
+        dict: .env file data
+    '''
+
+    data = dict()
+    with open(file, encoding='utf-8') as f:
+        for line in f:
+            re_data = search(r'([a-zA-Z0-9_]+)\=(.*\n(?=[A-Z])|.*$)', line)
+            var = re_data.group(1)
+            value = re_data.group(2).strip('{}{}'.format('"', "'"))
+            if value.isdigit():
+                data.update({var: int(value)})
+            elif value.lower() in ['true', 'false']:
+                if value.lower() == 'true':
+                    data.update({var: True})
+                else:
+                    data.update({var: False})
+            else:
+                data.update({var: value})
+    return(data)
