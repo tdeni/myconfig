6023c091558689d7e22ac8dd0268911b7a3f36fa
Deni <29742498+azureswastika@users.noreply.github.com>
2020-11-07 23:54:09 +0300

    Add MyConfig test

---
+++ tests/test_config.py
@@ -0,0 +1,12 @@
+import json
+from myconfig import MyConfig
+
+
+def test_config():
+    data = {'var1': 'val1', 'var2': 'val2'}
+    path = 'tests/data.json'
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+
+    config = MyConfig([path])
+    assert config.var1 == data.get('var1')
